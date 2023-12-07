f03d4c6e710da50af024dabe2f8094e056fe3c77
malikobansa <hackobansa@gmail.com>
2023-12-07 20:21:24 +0100

    fixed bug

--- src/components/Services/FetchAPI.js
+++ src/components/Services/FetchAPI.js
@@ -1,41 +1,56 @@
-import React,{ useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import './FetchAPI.css'
 
 export default function FetchAPI() {
-    const [news,setNews] = useState([]);
+    const [news, setNews] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-    useEffect(()=> {
+    useEffect(() => {
         const apiKey = '5923c983e11d4d01a29b697669f485a4';
         const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`;
 
         fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            setNews(data.articles)
-        })
-        .catch(error => {
-            console.error('Error fetching news:', error);
-        });
-    }, [])
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then(data => {
+                setNews(data.articles);
+                setIsLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching news:', error);
+                setError(error);
+                setIsLoading(false);
+            });
+    }, []);
 
-  return (
-    <div className='fetch-container'>
-        <div className='fetch'>
-            <a href='/news'>
-            {news[0] && <img src={news[0].urlToImage} className='img' alt={news[0].title}/>}        
-            </a>
-        </div>
-        <div className='fetch text'>
-            <a href='/news' className='news'>
-            {news[0] && (
-                <div className='text name' style={{marginTop:"130px"}}>
-                    <h1>{news[0].source.name}</h1>
-                    <h2>{news[0].title}</h2>
-                    <p>{news[0].description}</p>
+    if (isLoading) return <div className='loading'>Loading...</div>;
+    if (error) return <div className='error'>Error fetching news: {error.message}</div>;
+
+    return (
+        <div className='fetch-container'>
+            {news.length > 0 && (
+                <div className='fetch'>
+                    <a href='/news'>
+                        <img src={news[0].urlToImage} className='img' alt={news[0].title}/>
+                    </a>
                 </div>
             )}
-            </a>
+            {news.length > 0 && (
+                <div className='fetch text'>
+                    <a href='/news' className='news'>
+                        <div className='text name' style={{marginTop:"130px"}}>
+                            <h1>{news[0]?.source.name}</h1>
+                            <h2>{news[0]?.title}</h2>
+                            <p>{news[0]?.description}</p>
+                        </div>
+                    </a>
+                </div>
+            )}
         </div>
-    </div>
-  )
+    )
 }
